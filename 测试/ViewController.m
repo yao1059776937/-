5d2b050998d49332a51407d8e46fa46553868db9
mac--姚应龙 <1059776937@qq.com>
2016-02-26 11:00:24 +0800

    第一次

--- 测试/ViewController.m
+++ 测试/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"123213435");
 }
 
 - (void)didReceiveMemoryWarning {
